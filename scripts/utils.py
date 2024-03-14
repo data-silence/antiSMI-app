f03c56b542af6866293ddeb9d87fc5ffb1e683d6
data-silence <enjoy-ds@pm.me>
2024-03-14 21:30:00 +0300

    Total refactoring for utils.py

--- scripts/utils.py
+++ scripts/utils.py
@@ -1,3 +1,7 @@
+"""
+Used to receive and pre-process news for further design of streamlit application
+"""
+
 import pandas as pd
 import streamlit as st
 import datetime as dt
@@ -15,25 +19,16 @@ import random
 
 import torch
 from transformers import AutoTokenizer, AutoModel
+from scripts.constants import tm_start_date, tm_last_date, api_url, default_categories
 
 device = "cuda" if torch.cuda.is_available() else "cpu"
 
 tokenizer = AutoTokenizer.from_pretrained("cointegrated/LaBSE-en-ru")
 model = AutoModel.from_pretrained("cointegrated/LaBSE-en-ru").to(device)
 
-api_url = "http://127.0.0.1:8000"
-default_categories = ['economy', 'technology', 'entertainment', 'science', 'sports']
-categories_dict = {
-    'economy': {'emoj': '💰'},
-    'science': {'emoj': '🔬'},
-    'sports': {'emoj': '🏃'},
-    'technology': {'emoj': '📲'},
-    'entertainment': {'emoj': '👻'},
-    'society': {'emoj': '👲'}
-}
-
-tm_start_date = dt.date(1999, 8, 31)
-tm_last_date = dt.date(2023, 12, 31)
+"""
+One-off scripts
+"""
 
 
 def select_random_date():
@@ -41,62 +36,13 @@ def select_random_date():
     return tm_start_date + dt.timedelta(random.randint(0, delta.days))
 
 
-@st.cache_data
-def get_df_from_asmi(handler: str) -> pd.DataFrame:
-    """Converts json received from API to dataframe"""
-    handler_url = f"{api_url}{handler}"
-    # print(handler_url)
-    response = requests.get(handler_url).json()
-    json_dump = json.dumps(response)
-    df = pd.read_json(StringIO(json_dump))
-    return df
-
-
-@st.cache_data
-def get_date_df_from_tm(start_date: datetime.date, end_date: datetime.date) -> pd.DataFrame:
-    """Converts json received from API to dataframe"""
-    handler_url = f"{api_url}/news/tm/{str(start_date)}/{str(end_date)}"
-    print(handler_url)
-    response = requests.get(handler_url).json()
-    json_dump = json.dumps(response)
-    df = pd.read_json(StringIO(json_dump))
-    return df
-
-
-def get_clusters_columns(start_date: datetime.date, end_date: datetime.date) -> pd.DataFrame:
-    df = get_date_df_from_tm(start_date=start_date, end_date=end_date)
-
-    if len(df) > 1:  # кластеризацию возможно сделать только если количество новостей более одной
-        model = AgglomerativeClustering(n_clusters=None, metric='cosine', linkage='complete',
-                                        distance_threshold=0.3)
-        labels = model.fit_predict(list(df.embedding))
-        df.loc[:, 'label'] = labels
-
-    elif len(df) == 1:  # если новость одна - присваиваем ей лейбл = -1
-        df.loc[:, 'label'] = -1
-    # чтобы избежать отнесения одной новости по разным категориям, присвоим одному лейблу наиболее частую категорию,
-    trans = df.groupby(by=['label'])['category'].agg(pd.Series.mode)
-    df['new'] = df.label.apply(lambda x: trans.iloc[x])
-    # Оставляем только одно значение, если мода выдаёт несколько значений в np.ndarray
-    df.loc[:, 'new'] = df.new.apply(lambda x: x[0] if isinstance(x, np.ndarray) else x)
-
-    # Удаляем вспомогательные столбцы и сортируем
-    df.drop(columns='category', inplace=True)
-    df.rename(columns={'new': 'category'}, inplace=True)
-    df.sort_values(by=['category', 'label'], ascending=True, inplace=True)
-
-    return df
-
-
-@st.cache_data
-def filter_df(df: pd.DataFrame, amount: int, categories: list) -> pd.DataFrame:
-    df = df.loc[df['category'].isin(categories)]
-    final_labels = []
-    for category in categories:
-        most_tuple = Counter(list(df.label[df.category == category])).most_common(amount)
-        most_labels = [el[0] for el in most_tuple]
-        final_labels.extend(most_labels)
-    return df[df.label.isin(final_labels)]
+def make_single_embs(sentences):
+    encoded_input = tokenizer(sentences, padding=True, truncation=True, max_length=64, return_tensors='pt').to(device)
+    with torch.no_grad():
+        model_output = model(**encoded_input)
+    embeddings = model_output.pooler_output
+    embeddings = torch.nn.functional.normalize(embeddings)
+    return embeddings[0].tolist()
 
 
 def cos_simularity(a, b):
@@ -110,9 +56,113 @@ def find_sim_news(df: pd.DataFrame, q_emb: list[float]):
     return best_result
 
 
+"""
+From handlers interpreters
+"""
+
+
+@st.cache_data
+def get_df_from_asmi(handler: str) -> pd.DataFrame:
+    """Converts json received from asmi API-handlers to dataframe"""
+    handler_url = f"{api_url}{handler}"
+    response = requests.get(handler_url).json()
+    json_dump = json.dumps(response)
+    df = pd.read_json(StringIO(json_dump))
+    return df
+
+
+@st.cache_data
+def get_date_df_from_tm(start_date: datetime.date, end_date: datetime.date) -> pd.DataFrame:
+    """Converts json received from timemachine API-handlers to dataframe"""
+    handler_url = f"{api_url}/news/tm/{str(start_date)}/{str(end_date)}"
+    # print(handler_url)
+    response = requests.get(handler_url).json()
+    json_dump = json.dumps(response)
+    df = pd.read_json(StringIO(json_dump))
+    return df
+
+
+@st.cache_data
+def get_answer(query: str) -> pd.DataFrame:
+    """Sends sentence and get similar-news from API"""
+    handler_url = f"{api_url}/news/tm/get_similar_news"
+    embedding = make_single_embs(query)
+    response = requests.post(handler_url, json=embedding).json()
+    json_dump = json.dumps(response)
+    similar_news_df = pd.read_json(StringIO(json_dump))
+    similar_news_df.drop(columns='embedding', inplace=True)
+    return similar_news_df
+
+
+"""
+Mixin to preprocess the dataframe into the Service format
+"""
+
+
+class DataframeMixin:
+    @staticmethod
+    def get_dataframe(df_type: str, media_type: str = None, start_date: datetime.date = None,
+                      end_date: datetime.date = None) -> pd.DataFrame:
+        df = None
+        match df_type:
+            case 'asmi_brief':
+                df = get_df_from_asmi("/news/asmi/today/brief")
+                df['embedding'] = df['news'].apply(lambda x: make_single_embs(x))
+            case 'asmi_media_type':
+                df = get_df_from_asmi(f"/news/asmi/today/{media_type}")
+                df['embedding'] = df['news'].apply(lambda x: make_single_embs(x))
+            case 'tm':
+                df = get_date_df_from_tm(start_date=start_date, end_date=end_date)
+        return df
+
+    @staticmethod
+    def get_clusters_columns(df_type: str, media_type: str = None, start_date: datetime.date = None,
+                             end_date: datetime.date = None) -> pd.DataFrame:
+        df = DataframeMixin.get_dataframe(df_type=df_type, media_type=media_type, start_date=start_date,
+                                          end_date=end_date)
+
+        if len(df) > 1:  # clustering is possible only if the number of news items is more than one
+            model = AgglomerativeClustering(n_clusters=None, metric='cosine', linkage='complete',
+                                            distance_threshold=0.3)
+            labels = model.fit_predict(list(df.embedding))
+            df.loc[:, 'label'] = labels
+
+        elif len(df) == 1:  # if there is only one news item, we assign it a label = -1
+            df.loc[:, 'label'] = -1
+        # To avoid putting the same news in different categories, we assign the most frequent category to one label
+        trans = df.groupby(by=['label'])['category'].agg(pd.Series.mode)
+        df['new'] = df.label.apply(lambda x: trans.iloc[x])
+        # Leave only one value if the mod produces multiple values in np.ndarray
+        df.loc[:, 'new'] = df.new.apply(lambda x: x[0] if isinstance(x, np.ndarray) else x)
+
+        # Removing auxiliary columns and sorting
+        df.drop(columns='category', inplace=True)
+        df.rename(columns={'new': 'category'}, inplace=True)
+        df.sort_values(by=['category', 'label'], ascending=True, inplace=True)
+        return df
+
+    @staticmethod
+    def filter_df(df: pd.DataFrame, amount: int, categories: list) -> pd.DataFrame:
+        df = df.loc[df['category'].isin(categories)]
+        final_labels = []
+        for category in categories:
+            most_tuple = Counter(list(df.label[df.category == category])).most_common(amount)
+            most_labels = [el[0] for el in most_tuple]
+            final_labels.extend(most_labels)
+        return df[df.label.isin(final_labels)]
+
+
+"""
+News Services 
+"""
+
+
 @dataclass
-class NewsService:
-    service_name: str = 'asmi'
+class TimemachineService:
+    """
+    Class to handle news dataframes into news-digest for Timemachine service
+    """
+    service_name: str = 'tm'
     start_date: dt.date = None
     end_date: dt.date = start_date
     news_amount: int = 3
@@ -122,21 +172,19 @@ class NewsService:
 
     def __post_init__(self):
         self.categories = default_categories
-        # self.date_df = get_clusters_columns(start_date=self.start_date, end_date=self.end_date)
-        # self.most_df = filter_df(self.date_df, amount=self.news_amount, categories=self.categories)
 
     def set_params(self, start_date: dt.date, end_date: dt.date, news_amount: int, categories: list[str]):
         if self.date_df is None:
-            self.date_df = get_clusters_columns(start_date=start_date, end_date=end_date)
+            self.date_df = DataframeMixin.get_clusters_columns(df_type=self.service_name, start_date=start_date,
+                                                               end_date=end_date)
         compare_params = ((self.start_date, self.end_date, self.news_amount, self.categories) ==
                           (start_date, end_date, news_amount, categories))
 
         if not compare_params:
             if not (self.start_date, self.end_date) == (start_date, end_date):
-                self.date_df = get_clusters_columns(start_date=start_date, end_date=end_date)
-            self.most_df = filter_df(self.date_df, amount=news_amount, categories=categories)
-        # self.date_df = get_clusters_columns(start_date=start_date, end_date=end_date)
-        # self.most_df = filter_df(self.date_df, amount=news_amount, categories=categories)
+                self.date_df = DataframeMixin.get_clusters_columns(df_type=self.service_name, start_date=start_date,
+                                                                   end_date=end_date)
+            self.most_df = DataframeMixin.filter_df(self.date_df, amount=news_amount, categories=categories)
         self.start_date, self.end_date, self.news_amount, self.categories = start_date, end_date, news_amount, categories
 
     def get_source_links(self, title: str):
@@ -167,70 +215,28 @@ class NewsService:
         my_news = {}
         for category in result_df.category.unique():
             category_df = result_df[result_df.category == category]
-            # category_df.links = category_df.links.apply(lambda x: x.split())
             category_list = list(
                 zip(category_df['date'], category_df['title'], category_df['resume'], category_df['links']))
             my_news.update({category: category_list})
         return my_news
 
 
-def make_single_embs(sentences):
-    encoded_input = tokenizer(sentences, padding=True, truncation=True, max_length=64, return_tensors='pt').to(device)
-    with torch.no_grad():
-        model_output = model(**encoded_input)
-    embeddings = model_output.pooler_output
-    embeddings = torch.nn.functional.normalize(embeddings)
-    return embeddings[0].tolist()
-
-
-@st.cache_data
-def get_today_emb():
-    """Converts json received from API to dataframe"""
-    handler_url = f"{api_url}/news/asmi/today/brief"
-    response = requests.get(handler_url).json()
-    json_dump = json.dumps(response)
-    df = pd.read_json(StringIO(json_dump))
-    df['embedding'] = df['news'].apply(lambda x: make_single_embs(x))
-    return df
-
-
-def get_clusters_columns_2() -> pd.DataFrame:
-    df = get_today_emb()
-
-    if len(df) > 1:  # кластеризацию возможно сделать только если количество новостей более одной
-        model = AgglomerativeClustering(n_clusters=None, metric='cosine', linkage='complete',
-                                        distance_threshold=0.3)
-        labels = model.fit_predict(list(df.embedding))
-        df.loc[:, 'label'] = labels
-
-    elif len(df) == 1:  # если новость одна - присваиваем ей лейбл = -1
-        df.loc[:, 'label'] = -1
-    # чтобы избежать отнесения одной новости по разным категориям, присвоим одному лейблу наиболее частую категорию,
-    trans = df.groupby(by=['label'])['category'].agg(pd.Series.mode)
-    df['new'] = df.label.apply(lambda x: trans.iloc[x])
-    # Оставляем только одно значение, если мода выдаёт несколько значений в np.ndarray
-    df.loc[:, 'new'] = df.new.apply(lambda x: x[0] if isinstance(x, np.ndarray) else x)
-
-    # Удаляем вспомогательные столбцы и сортируем
-    df.drop(columns='category', inplace=True)
-    df.rename(columns={'new': 'category'}, inplace=True)
-    df.sort_values(by=['category', 'label'], ascending=True, inplace=True)
-
-    return df
-
-
 @dataclass
 class AsmiService:
+    """
+    Class to handle news dataframes into news-digest for AsmiService service
+    """
+    service_name: str = 'asmi_brief'
     news_amount: int = 3
     categories: list[str] = field(default_factory=list)
 
     def __post_init__(self):
         self.categories = default_categories
-        self.date_df = get_clusters_columns_2()
-        self.most_df = filter_df(self.date_df, amount=self.news_amount, categories=self.categories)
+        self.date_df = DataframeMixin.get_clusters_columns(df_type=self.service_name)
+        self.most_df = DataframeMixin.filter_df(self.date_df, amount=self.news_amount, categories=self.categories)
 
     def set_params(self, news_amount: int, categories: list[str]):
-        self.most_df = filter_df(self.date_df, amount=news_amount, categories=categories)
+        self.most_df = DataframeMixin.filter_df(self.date_df, amount=news_amount, categories=categories)
         self.news_amount, self.categories = news_amount, categories
 
     def get_source_links(self, title: str):
@@ -261,69 +267,12 @@ class AsmiService:
         my_news = {}
         for category in result_df.category.unique():
             category_df = result_df[result_df.category == category]
-            # category_df.links = category_df.links.apply(lambda x: x.split())
             category_list = list(
                 zip(category_df['date'], category_df['title'], category_df['resume'], category_df['links']))
             my_news.update({category: category_list})
         return my_news
 
 
-@st.cache_data
-def get_answer(query: str) -> pd.DataFrame:
-    """Converts json received from API to dataframe"""
-    handler_url = f"{api_url}/news/tm/get_similar_news"
-    embedding = make_single_embs(query)
-    response = requests.post(handler_url, json=embedding).json()
-    json_dump = json.dumps(response)
-    similar_news_df = pd.read_json(StringIO(json_dump))
-    similar_news_df.drop(columns='embedding', inplace=True)
-    return similar_news_df
-
-
-major_events = {
-    1: {'start_date': dt.datetime(1999, 12, 31), 'end_date': dt.datetime(2000, 1, 3)},
-    2: {'start_date': dt.datetime(2000, 3, 25), 'end_date': dt.datetime(2000, 3, 27)},
-    3: {'start_date': dt.datetime(2000, 8, 12), 'end_date': dt.datetime(2000, 8, 15)},
-    4: {'start_date': dt.datetime(2001, 9, 11), 'end_date': dt.datetime(2001, 9, 15)},
-    5: {'start_date': dt.datetime(2002, 10, 23), 'end_date': dt.datetime(2002, 10, 26)},
-    6: {'start_date': dt.datetime(2003, 10, 25), 'end_date': dt.datetime(2003, 10, 30)},
-    7: {'start_date': dt.datetime(2004, 12, 25), 'end_date': dt.datetime(2004, 12, 27)},
-    8: {'start_date': dt.datetime(2007, 12, 10), 'end_date': dt.datetime(2007, 12, 17)},
-    9: {'start_date': dt.datetime(2008, 8, 7), 'end_date': dt.datetime(2008, 8, 12)},
-    10: {'start_date': dt.datetime(2011, 9, 24), 'end_date': dt.datetime(2011, 9, 27)},
-    11: {'start_date': dt.datetime(2011, 12, 4), 'end_date': dt.datetime(2011, 12, 6)},
-    12: {'start_date': dt.datetime(2012, 5, 6), 'end_date': dt.datetime(2012, 5, 12)},
-    13: {'start_date': dt.datetime(2014, 2, 22), 'end_date': dt.datetime(2014, 3, 1)},
-}
-
-# @st.cache_data
-# def get_answer(query: str, start_date: datetime.date, end_date: datetime.date, news_amount: int,
-#                category: str) -> pd.DataFrame:
-#     """Converts json received from API to dataframe"""
-#     handler_url = f"{api_url}/news/tm/get_similar_news/{start_date}/{end_date}?news_amount={news_amount}&category={category}"
-#     embedding = make_single_embs(query)
-#     response = requests.post(handler_url, json=embedding).json()
-#     json_dump = json.dumps(response)
-#     similar_news_df = pd.read_json(StringIO(json_dump))
-#     return similar_news_df
-
-
-# @st.cache_data
-# def get_df_from_handlers_response(handler: str) -> pd.DataFrame:
-#     """Converts json received from API to dataframe"""
-#     handler_url = f"{api_url}{handler}"
-#     response = requests.post(handler_url).json()
-#     json_dump = json.dumps(response)
-#     df = pd.read_json(StringIO(json_dump))
-#     return df
-
-
 if __name__ == "__main__":
-    # df = get_df_from_handlers_response("last_quota")
-    # print(df.head())
-    # test_url = f"{api_url}/api/generate"
-    # response = requests.post('{"model": "llama2", "prompt": "Why is the sky blue?"}')
-    # today_service = AsmiService()
-    # print(today_service.digest_df())
-    # pass
-    print(get_word_emb('Что пообещал Путин?'))
+    df = get_df_from_asmi("/news/asmi/today/brief")
+    print(df.head())
